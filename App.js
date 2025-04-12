e6615befd18f2c84b60bc4e5ab4ee29f388f80d4
NandiniNewar <119478079+NandiniNewar@users.noreply.github.com>
2025-04-12 09:58:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import ButtonClick from "./ButtonClick"; 
+const App = () => {
+  return (
+    <div>
+      <h1>React Event Handling</h1>
+      <ButtonClick />
+    </div>
+  );
+};
+
+export default App;
